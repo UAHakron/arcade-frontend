cde0de765da1a3cfd4ad2e00cde8ef8d83eb5ea5
Kyle Vasulka <kjv13@zips.uakron.edu>
2017-09-22 23:24:51 -0400

    changed get request location again for api

--- PythonScripts/Arcade2.0/ClientGUITest.py
+++ PythonScripts/Arcade2.0/ClientGUITest.py
@@ -46,17 +46,17 @@ def scanTag():
             #API stuff
             
             #get request on email
-            email_exists = requests.get('https://hakron.io/arcade/people/'+str(email_text.get()))
+            email_exists = requests.get('https://hakron.io/arcade/api/people/'+str(email_text.get()))
             
                 #if email exists
             if email_exists.status_code == 200:
                 user_data = json.loads(email_exists.json())
-                update_id = requests.post("https:/hakron.io/arcade/tags/" + str(hex(uid[0]))+"."+str(hex(uid[1]))+"."+str(hex(uid[2]))+"."+str(hex(uid[3])) + "/people", json={"_id":user_data["_id"]})
+                update_id = requests.post("https:/hakron.io/arcade/api/tags/" + str(hex(uid[0]))+"."+str(hex(uid[1]))+"."+str(hex(uid[2]))+"."+str(hex(uid[3])) + "/people", json={"_id":user_data["_id"]})
                 #if email does not exist
             else:
-                create_user = requests.post("https://hakron.io/arcade/people", json={"name":str(name_text.get()),"nfc":str(hex(uid[0]))+"."+str(hex(uid[1]))+"."+str(hex(uid[2]))+"."+str(hex(uid[3])),"email":str(email_text.get()) ,"bits":"0"})
+                create_user = requests.post("https://hakron.io/arcade/api/people", json={"name":str(name_text.get()),"nfc":str(hex(uid[0]))+"."+str(hex(uid[1]))+"."+str(hex(uid[2]))+"."+str(hex(uid[3])),"email":str(email_text.get()) ,"bits":"0"})
                 user_data = json.loads(create_user.json())
-                update_id = requests.post("https:/hakron.io/arcade/tags/" + str(hex(uid[0]))+"."+str(hex(uid[1]))+"."+str(hex(uid[2]))+"."+str(hex(uid[3])) + "/people", json={"_id":user_data["_id"]})
+                update_id = requests.post("https:/hakron.io/arcade/api/tags/" + str(hex(uid[0]))+"."+str(hex(uid[1]))+"."+str(hex(uid[2]))+"."+str(hex(uid[3])) + "/people", json={"_id":user_data["_id"]})
                 
             #
 
